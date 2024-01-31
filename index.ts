974fd1335374ceac09cfaa91ff0bfa14bfac29c7
ih.zonaid <ahammedzonaid@gmail.com>
2024-01-31 12:27:43 +0600

    exporting neccesery functions.

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 import ConnectDialog from "./src/components/connect_dialog";
 import ConnectWalletButton from "./src/components/connet_wallet_button";
+import { clientsign } from "./src/lib/stellar/utils";
+import { useConnectWalletStateStore } from "./src/state/connect_wallet_state";
 
-export { ConnectWalletButton, ConnectDialog };
+export {
+  ConnectWalletButton,
+  ConnectDialog,
+  clientsign,
+  useConnectWalletStateStore,
+};
