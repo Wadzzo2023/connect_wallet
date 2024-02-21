ebe2bde0cdfb40c4a80f3c01480b1281bde57b4f
ih.zonaid <ahammedzonaid@gmail.com>
2024-02-21 19:39:00 +0600

    add logout btn

--- src/components/connet_wallet_button.tsx
+++ src/components/connet_wallet_button.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { useDialogStore } from "../state/connect_wallet_dialog";
 import { useConnectWalletStateStore } from "../state/connect_wallet_state";
 import { addrShort } from "../lib/utils";
+import { LogOut } from "lucide-react";
+import { signOut } from "next-auth/react";
 
 export default function ConnectWalletButton() {
   const walletState = useConnectWalletStateStore();
@@ -14,20 +16,38 @@ export default function ConnectWalletButton() {
 
   const setDialog = useDialogStore();
   return (
-    <button onClick={() => setDialog.setIsOpen(true)}>
-      <div className="tsd btn flex items-center justify-center rounded-xl border border-slate-50/10 bg-base-200 px-4 py-2 text-sm font-bold tracking-wider text-gray-700 backdrop-blur hover:scale-95 hover:border-blue-800 hover:bg-blue-800 hover:text-blue-100 focus:border-blue-700 active:ring-blue-500 ">
-        <div className="relative mr-2 h-6 w-6">
-          <Image
-            alt="BandCoin logo"
-            layout="fill"
-            objectFit="cover"
-            src="/favicon.ico"
-          />
+    <div className="flex items-center gap-2 ">
+      <button onClick={() => setDialog.setIsOpen(true)} className="flex-1">
+        <div className="tsd btn flex items-center justify-center rounded-xl border border-slate-50/10 bg-base-200 px-4 py-2 text-sm font-bold tracking-wider text-gray-700 backdrop-blur hover:scale-95 hover:border-blue-800 hover:bg-blue-800 hover:text-blue-100 focus:border-blue-700 active:ring-blue-500 ">
+          <div className="relative mr-2 h-6 w-6">
+            <Image
+              alt="BandCoin logo"
+              layout="fill"
+              objectFit="cover"
+              src="/favicon.ico"
+            />
+          </div>
+          <span className="text-base-content">
+            {isWalletAva ? addrShort(walletState.pubkey) : "Connect Wallet"}
+          </span>
         </div>
-        <span className="text-base-content">
-          {isWalletAva ? addrShort(walletState.pubkey) : "Connect Wallet"}
-        </span>
-      </div>
+      </button>
+      {isWalletAva && <LogOutButon />}
+    </div>
+  );
+}
+
+function LogOutButon() {
+  const walletState = useConnectWalletStateStore();
+   async function disconnectWallet() {
+     await signOut({
+       redirect: false,
+     });
+     walletState.removeUserDat();
+   }
+  return (
+    <button className="btn btn-circle" onClick={disconnectWallet}>
+      <LogOut />
     </button>
   );
 }
