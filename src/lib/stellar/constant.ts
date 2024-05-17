872cb96adfd293cd577c57f743f84db87300d9d5
Zonaid <ahammedzonaid@gmail.com>
2024-05-17 11:55:27 +0600

    action url change

--- src/lib/stellar/constant.ts
+++ src/lib/stellar/constant.ts
@@ -8,4 +8,6 @@ export const STELLAR_URL = process.env.NEXT_PUBLIC_STELLAR_PUBNET
   ? "https://horizon.stellar.org"
   : "https://horizon-testnet.stellar.org";
 
-export const USER_ACOUNT_URL = "https://accounts.action-tokens.com/api/account";
+export const ACTION_STELLAR_ACCOUNT_URL = "https://accounts.action-tokens.com/";
+
+export const USER_ACOUNT_URL = ACTION_STELLAR_ACCOUNT_URL + "api/account";
