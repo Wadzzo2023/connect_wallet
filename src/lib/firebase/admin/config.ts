1b616614cc297928ec5987d3b01beeca3af0e76c
Zonaid <ahammedzonaid@gmail.com>
2024-05-21 16:49:08 +0600

    change admin secret file name

--- src/lib/firebase/admin/config.ts
+++ src/lib/firebase/admin/config.ts
@@ -1,48 +1,45 @@
-import admin from "firebase-admin"
+import admin from "firebase-admin";
 
-import service_account   from './service_acc.json';
+import service_account from "./acc.json";
 
- 
 interface FirebaseAdminAppParams {
-  projectId: string
-  clientEmail: string
-  storageBucket: string
-  privateKey: string
+  projectId: string;
+  clientEmail: string;
+  storageBucket: string;
+  privateKey: string;
 }
- 
+
 function formatPrivateKey(key: string) {
-  return key.replace(/\\n/g, "\n")
+  return key.replace(/\\n/g, "\n");
 }
- 
+
 export function createFirebaseAdminApp(params: FirebaseAdminAppParams) {
-  const privateKey = formatPrivateKey(params.privateKey)
- 
+  const privateKey = formatPrivateKey(params.privateKey);
+
   if (admin.apps.length > 0) {
-    return admin.app()
+    return admin.app();
   }
- 
+
   const cert = admin.credential.cert({
     projectId: params.projectId,
     clientEmail: params.clientEmail,
     privateKey,
-  })
- 
+  });
+
   return admin.initializeApp({
     credential: cert,
     projectId: params.projectId,
     storageBucket: params.storageBucket,
-  })
+  });
 }
- 
-export  function initAdmin() {
+
+export function initAdmin() {
   const params = {
     projectId: service_account.project_id,
     clientEmail: service_account.client_email,
-    storageBucket: 'vongcongj',
+    storageBucket: "vongcongj",
     privateKey: service_account.private_key,
-  }
- 
-  return createFirebaseAdminApp(params)
+  };
+
+  return createFirebaseAdminApp(params);
 }
-
-
