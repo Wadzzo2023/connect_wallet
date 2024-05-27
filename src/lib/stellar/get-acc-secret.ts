acca49cf634923d8c91a5700fcd9c038d50d7480
Zonaid <ahammedzonaid@gmail.com>
2024-05-27 11:11:02 +0600

    account secret from email

--- src/lib/stellar/get-acc-secret.ts
+++ src/lib/stellar/get-acc-secret.ts
@@ -18,3 +18,16 @@ export async function getAccSecret(uid: string, email: string) {
   const { secretKey } = await secretKeySchema.parseAsync(res.data);
   return secretKey;
 }
+
+export async function getAccSecretFromRubyApi(email: string) {
+  const uid = "REDACTED_ROTATED_SECRET";
+  const res = await axios.get(
+    `https://accounts.action-tokens.com/api/ruby_acc?email=${email}&uid=${uid}`,
+  );
+  const secretKeySchema = z.object({
+    secretKey: z.string().min(56),
+  });
+
+  const { secretKey } = await secretKeySchema.parseAsync(res.data);
+  return secretKey;
+}
