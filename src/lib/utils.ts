26ea30a139e68e04207e3bbbb15c41eeb3c5ab56
Zonaid <ahammedzonaid@gmail.com>
2024-05-29 18:27:41 +0600

    enum changed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,7 @@ import { twMerge } from "tailwind-merge";
 import { WalletType } from "./enums";
 
 export function addrShort(addr: string, size?: number) {
+  if (!addr) return "No address";
   const cropSize = size ?? 3;
   return `${addr.substring(0, cropSize)}...${addr.substring(addr.length - cropSize, addr.length)}`;
 }
