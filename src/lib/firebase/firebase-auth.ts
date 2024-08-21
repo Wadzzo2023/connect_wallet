637c7697dbe071f5b7d193ee1d5e0784d5fd776b
Action <admin@actn.xyz>
2024-08-21 23:49:31 +0600

    update firebase admin secret

--- src/lib/firebase/firebase-auth.ts
+++ src/lib/firebase/firebase-auth.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBL_4Qgg2pqtLgYiHunyKze_BLMHNQV3u4",
-  authDomain: "oauth.action-tokens.com",
-  projectId: "auth-29d94",
-  storageBucket: "auth-29d94.appspot.com",
-  messagingSenderId: "443284916220",
-  appId: "1:443284916220:web:3be7d960eab4c329969479",
-}
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_AUTH_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_AUTH_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_AUTH_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_AUTH_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_AUTH_APP_ID!,
+};
 
 // Initialize Firebase
-const authApp = initializeApp(firebaseConfig, "authApp")
-export const auth = getAuth(authApp)
+const authApp = initializeApp(firebaseConfig, "authApp");
+export const auth = getAuth(authApp);
