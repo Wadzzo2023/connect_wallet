793cd3e7596be8eb3d664871d606e88ad117c057
sheikhfoysaldiu <sheikhfoysal2025@gmail.com>
2024-09-25 18:06:07 +0600

    TrxBaseFee +FeeInPlatformAsset + Platform Fee Fix

--- src/lib/stellar/constant.ts
+++ src/lib/stellar/constant.ts
@@ -15,13 +15,16 @@ export const USER_ACCOUNT_URL = ACTION_STELLAR_ACCOUNT_URL + "api/account";
 export const USER_ACCOUNT_XDR_URL =
   ACTION_STELLAR_ACCOUNT_URL + "api/account_xdr";
 
-export const TrxBaseFee = "1000";
-export const TrxBaseFeeInPlatformAsset = "1400";
-export const PLATFORM_FEE = "3000"; // in platform asset
 
 export const PLATFORM_ASSET = new Asset(
   env.NEXT_PUBLIC_ASSET_CODE,
   env.NEXT_PUBLIC_ASSET_ISSUER,
 );
+
+export const TrxBaseFee = "1000";
+export const TrxBaseFeeInPlatformAsset = PLATFORM_ASSET.code.toLowerCase() === "wadzzo" ? "10" : "1400"
+export const PLATFORM_FEE = PLATFORM_ASSET.code.toLowerCase() === "wadzzo" ? "25" : "6000"
+
+
 
 export const STROOP = "0.0000001";
