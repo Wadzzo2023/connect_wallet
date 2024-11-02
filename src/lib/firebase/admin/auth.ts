2dccfe66fbfb1b901b668ba792d9457c2de98154
Action <admin@actn.xyz>
2024-11-02 19:50:00 +0600

    apple provider

--- src/lib/firebase/admin/auth.ts
+++ src/lib/firebase/admin/auth.ts
@@ -1,3 +1,5 @@
+import { FirebaseError } from "firebase/app";
+import { verifyAppleToken } from "./apple-verify";
 import { initAdmin } from "./config";
 
 export async function getAdminUserInfo(email: string) {
@@ -14,7 +16,49 @@ export async function getAdminUserInfo(email: string) {
 export async function verifyIdToken(token: string) {
   const admin = initAdmin();
   const decodedToken = await admin.auth().verifyIdToken(token);
-  return decodedToken.uid;
+  return decodedToken;
+}
+
+export async function appleTokenToUserProvideUid(appleToken: string) {
+  const appleUser = (await verifyAppleToken(appleToken)) as {
+    email: string;
+    sub: string;
+    name: string;
+  };
+
+  const admin = initAdmin();
+  const auth = admin.auth();
+
+  try {
+    // Try to get existing user first
+    const firebaseUser = await auth.getUserByProviderUid(
+      "apple.com",
+      appleUser.sub,
+    );
+
+    return appleUser.sub;
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      if (error.code === "auth/user-not-found") {
+        // Create new Firebase user if they don't exist
+        const newUser = await auth.createUser({
+          email: appleUser.email,
+          displayName: appleUser.name,
+        });
+
+        const user = await admin.auth().updateUser(newUser.uid, {
+          providerToLink: {
+            uid: appleUser.sub,
+            providerId: "apple.com",
+            email: appleUser.email,
+          },
+        });
+        return appleUser.sub;
+      }
+    }
+    console.log(error);
+    throw error;
+  }
 }
 
 export async function getEmailviaUID(uid: string) {
--- src/lib/stellar/constant.ts
+++ src/lib/stellar/constant.ts
@@ -1,27 +1,35 @@
 import { Asset, Networks } from "@stellar/stellar-sdk";
 import { env } from "~/env";
 
-export const networkPassphrase = Networks.PUBLIC
+export const networkPassphrase = Networks.PUBLIC;
 
-
-export const STELLAR_URL = "https://horizon.stellar.org"
-
+export const STELLAR_URL = "https://horizon.stellar.org";
 
 export const ACTION_STELLAR_ACCOUNT_URL = "https://accounts.action-tokens.com/";
 
 export const USER_ACCOUNT_URL = ACTION_STELLAR_ACCOUNT_URL + "api/account";
+export const USER_ACCOUNT_URL_APPLE =
+  ACTION_STELLAR_ACCOUNT_URL + "api/apple_private";
 export const USER_ACCOUNT_XDR_URL =
   ACTION_STELLAR_ACCOUNT_URL + "api/account_xdr";
-
 
 export const PLATFORM_ASSET = new Asset(
   env.NEXT_PUBLIC_ASSET_CODE,
   env.NEXT_PUBLIC_ASSET_ISSUER,
 );
 
-export const TrxBaseFee = env.NEXT_PUBLIC_STAGE === 'prod' ? "1000" : "100"; // 100
-export const TrxBaseFeeInPlatformAsset = env.NEXT_PUBLIC_STAGE === 'prod' ? (PLATFORM_ASSET.code.toLowerCase() === "wadzzo" ? "10" : "1400") : "1"
-export const PLATFORM_FEE = env.NEXT_PUBLIC_STAGE === 'prod' ? (PLATFORM_ASSET.code.toLowerCase() === "wadzzo" ? "25" : "6000") : "1"
-
+export const TrxBaseFee = env.NEXT_PUBLIC_STAGE === "prod" ? "1000" : "100"; // 100
+export const TrxBaseFeeInPlatformAsset =
+  env.NEXT_PUBLIC_STAGE === "prod"
+    ? PLATFORM_ASSET.code.toLowerCase() === "wadzzo"
+      ? "10"
+      : "1400"
+    : "1";
+export const PLATFORM_FEE =
+  env.NEXT_PUBLIC_STAGE === "prod"
+    ? PLATFORM_ASSET.code.toLowerCase() === "wadzzo"
+      ? "25"
+      : "6000"
+    : "1";
 
 export const STROOP = "0.0000001";
