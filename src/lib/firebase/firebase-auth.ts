68d1aabe84da3c9b2deb88738d047cd13fbd8a04
Foysal Sheikh <87323178+SheikhFoysaldiu@users.noreply.github.com>
2024-11-18 20:45:36 +0600

    storage

--- src/lib/firebase/firebase-auth.ts
+++ src/lib/firebase/firebase-auth.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,4 +17,5 @@ export const firebaseConfig = {
 
 // Initialize Firebase
 const authApp = initializeApp(firebaseConfig, "authApp");
+export const storage = getStorage(authApp);
 export const auth = getAuth(authApp);
