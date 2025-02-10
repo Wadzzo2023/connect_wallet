ba83984c18b51b2c82a6b5c916f9e7d8b77ba18d
IH <ahammedzonaid@gmail.com>
2025-02-10 15:58:42 +0600

    sumit trx

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import ConnectDialog from "./src/components/connect_dialog";
 import ConnectWalletButton from "./src/components/connet_wallet_button";
 import { WalletType } from "./src/lib/enums";
 import { getAccSecret } from "./src/lib/stellar/get-acc-secret";
+import { submitSignedXDRToServer4User } from "./src/lib/stellar/trx/payment_fb_g";
 import { clientsign, submitSignedXDRToServer } from "./src/lib/stellar/utils";
 
 export {
@@ -11,4 +12,5 @@ export {
   getAccSecret,
   WalletType,
   submitSignedXDRToServer,
+  submitSignedXDRToServer4User,
 };
