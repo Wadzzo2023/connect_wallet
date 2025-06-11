28368eb4ef82ef6bc1364265afdd814759a8a202
action-token <support@bandtogether.io>
2025-06-11 19:05:01 +0600

    apple-login-fix

--- src/lib/firebase/admin/apple-verify.ts
+++ src/lib/firebase/admin/apple-verify.ts
@@ -16,6 +16,6 @@ export async function verifyAppleToken(identityToken: string) {
   // Verify token
   return jwt.verify(identityToken, publicKey, {
     algorithms: ["RS256"],
-    audience: "com.thebillboardapp.wadzzo", // Your app's identifier
+    audience: "com.actionverse.actiontokens", // Your app's identifier
   });
 }
