e37058aec709df67c8bb61124a0b8b6147f162f8
actiontokens <support@action-tokens.com>
2026-01-30 23:48:04 +0600

    ui update for beam

--- src/components/connect_dialog.tsx
+++ src/components/connect_dialog.tsx
@@ -279,7 +279,7 @@ export default function ConnectDialog({ className }: ConnectDialogProps) {
             <span className="w-full border-t" />
           </div>
           <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
+            <span className=" bg-background px-2 text-muted-foreground">Or continue with</span>
           </div>
         </div>
         <AllButtons />
