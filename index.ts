a5f78c70fe32c0ea485ff7a13172952fa8cdc001
fyron <support@wadzzo.com>
2026-08-28 16:09:38 +0600

    refactor: remove deprecated getAccSecret function and retain getAccSecretFromRubyApi

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 import ConnectDialog from "./src/components/connect_dialog";
 import ConnectWalletButton from "./src/components/connet_wallet_button";
 import { WalletType } from "./src/lib/enums";
-import { getAccSecret } from "./src/lib/stellar/get-acc-secret";
 import { clientsign, submitSignedXDRToServer } from "./src/lib/stellar/utils";
 
 export {
   ConnectWalletButton,
   ConnectDialog,
   clientsign,
-  getAccSecret,
   WalletType,
   submitSignedXDRToServer,
 };
--- src/lib/stellar/get-acc-secret.ts
+++ src/lib/stellar/get-acc-secret.ts
@@ -1,23 +1,7 @@
 import axios from "axios";
 import { z } from "zod";
 
-export async function getAccSecret(uid: string, email: string) {
-  const res = await axios.get(
-    "https://accounts.action-tokens.com/api/getAccSecret",
-    {
-      params: {
-        uid,
-        email,
-      },
-    },
-  );
-  const secretKeySchema = z.object({
-    secretKey: z.string().min(56),
-  });
 
-  const { secretKey } = await secretKeySchema.parseAsync(res.data);
-  return secretKey;
-}
 
 export async function getAccSecretFromRubyApi(email: string) {
   const uid = "REDACTED_ROTATED_SECRET";
